394ac0899ecfb14eb2cecb6979e68d2b41b2799d
popeyeio <szl199083@126.com>
2018-11-27 21:59:56 +0800

    add examples

---
+++ examples/mutation/mutation.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/popeyeio/gohbase/gen/hbase"
+	"github.com/popeyeio/gohbase/pool"
+)
+
+func main() {
+	hbasePool := NewHbasePool()
+	defer hbasePool.Close()
+
+	hbaseClient, err := hbasePool.Get()
+	if err != nil {
+		fmt.Printf("Get error - %v\n", err)
+		return
+	}
+	defer hbaseClient.Close()
+
+	events := []*Event{
+		&Event{
+			EventTime: "20181127",
+			Publisher: "popeye",
+		},
+	}
+	if err = MutateEvents(hbaseClient, events); err != nil {
+		fmt.Printf("MutateEvents error - %v\n", err)
+	}
+}
+
+type Event struct {
+	EventTime string
+	Publisher string
+}
+
+func (e *Event) ParseToHbase() *hbase.BatchMutation {
+	return &hbase.BatchMutation{
+		Row: hbase.Text(e.EventTime),
+		Mutations: []*hbase.Mutation{
+			NewMutation("c:event_time", e.EventTime),
+			NewMutation("c:publisher", e.Publisher),
+		},
+	}
+}
+
+func NewMutation(column, value string) *hbase.Mutation {
+	return &hbase.Mutation{
+		Column:     hbase.Text(column),
+		Value:      hbase.Text(value),
+		WriteToWAL: true,
+	}
+}
+
+func NewHbasePool() pool.Pool {
+	opts := []pool.Option{
+		pool.WithAddrs("10.6.6.6:6666", "10.8.8.8:8888"),
+		pool.WithUpdatePickerInterval(time.Second * 10),
+		pool.WithMaxActive(8),
+		pool.WithMaxIdle(8),
+		pool.WithIdleTimeout(time.Second * 5),
+		pool.WithCleanUpInterval(time.Second * 30),
+		pool.WithBlockMode(true),
+	}
+	return pool.NewPool(opts...)
+}
+
+func MutateEvents(cli pool.Client, events []*Event) error {
+	batches := make([]*hbase.BatchMutation, len(events))
+	for i, event := range events {
+		batches[i] = event.ParseToHbase()
+	}
+
+	return cli.MutateRows("event", batches, nil)
+}
---
+++ examples/scanner/scanner.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/popeyeio/gohbase/gen/hbase"
+	"github.com/popeyeio/gohbase/pool"
+)
+
+func main() {
+	hbasePool := NewHbasePool()
+	defer hbasePool.Close()
+
+	hbaseClient, err := hbasePool.Get()
+	if err != nil {
+		fmt.Printf("Get error - %v\n", err)
+		return
+	}
+	defer hbaseClient.Close()
+
+	events, err := ScanEvents(hbaseClient, "20181127", "20181128")
+	if err != nil {
+		fmt.Printf("ScanEvents error - %v\n", err)
+		return
+	}
+	fmt.Printf("events:%v\n", events)
+}
+
+type Event struct {
+	EventTime string
+	Publisher string
+}
+
+func (e *Event) ParseFromHbase(r *hbase.TRowResult_) {
+	e.EventTime = GetColumn(r, "c:event_time")
+	e.Publisher = GetColumn(r, "c:publisher")
+}
+
+func GetColumn(r *hbase.TRowResult_, c string) (v string) {
+	if tcell := r.Columns[c]; tcell != nil {
+		v = string(tcell.Value)
+	}
+	return
+}
+
+func NewHbasePool() pool.Pool {
+	opts := []pool.Option{
+		pool.WithAddrs("10.6.6.6:6666", "10.8.8.8:8888"),
+		pool.WithUpdatePickerInterval(time.Second * 10),
+		pool.WithMaxActive(8),
+		pool.WithMaxIdle(8),
+		pool.WithIdleTimeout(time.Second * 5),
+		pool.WithCleanUpInterval(time.Second * 30),
+		pool.WithBlockMode(true),
+	}
+	return pool.NewPool(opts...)
+}
+
+func ScanEvents(cli pool.Client, startRow, stopRow string) ([]*Event, error) {
+	tscan := &hbase.TScan{
+		StartRow: hbase.Text(startRow),
+		StopRow:  hbase.Text(stopRow),
+	}
+
+	scanID, err := cli.ScannerOpenWithScan("event", tscan, nil)
+	if err != nil {
+		return nil, err
+	}
+	defer cli.ScannerClose(scanID)
+
+	var events []*Event
+	for {
+		results, err := cli.ScannerGetList(scanID, 128)
+		if err != nil {
+			return nil, err
+		}
+		if len(results) == 0 {
+			return events, nil
+		}
+
+		for _, result := range results {
+			event := &Event{}
+			event.ParseFromHbase(result)
+			events = append(events, event)
+		}
+	}
+}
